7fd55aaabd7299ad4a54448893dcb23c6f2b7ce0
Lala Kumar <lalak4099@gmail.com>
2024-11-18 22:00:10 +0530

    added user controller

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
 app.use(express.json()); // parse req.body
 app.use(express.urlencoded({ extended: true })); //parse fromdata
 app.use(cookieParser()); //for protectRoutes middleware
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 export { app };
---
+++ backend/controller/user.controller.js
@@ -0,0 +1,53 @@
+import User from "../models/user.model.js";
+
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne(username).select("-password");
+
+    if (!user) return res.status(404).json({ error: "user not found!" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getting user profile", error.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const followUnFollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id).select("-password");
+    const currentUser = await User.findById(req.user._id).select("-password");
+
+    if (!userToModify || !currentUser) {
+      return res.status(404).json({ error: "user not found!" });
+    }
+
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "you can't follow unfollow yourself!" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      //unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ messsage: "Unfollow successfully!" });
+    } else {
+      //follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ messsage: "User Followed successfully!" });
+    }
+  } catch (error) {
+    console.log("Error in followUnFollowUser user profile", error.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const updateUserProfile = async (req, res) => {};
