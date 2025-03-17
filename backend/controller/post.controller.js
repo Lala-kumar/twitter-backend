91cee8b9a268292263f72ca6e4c79645e54fbd37
Lala Kumar <lalak4099@gmail.com>
2025-03-17 12:16:56 +0530

    added endpoints to retrieve posts from followed users and specific user posts

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -186,3 +186,61 @@ export const getLikedPost = async (req, res) => {
     return res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getFollowingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    return res.status(200).json(feedPosts);
+  } catch (error) {
+    console.log("get following post: ", error.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    return res.status(200).json(posts);
+  } catch (error) {
+    console.log("get user post: ", error.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -7,11 +7,15 @@ import {
   getAllPost,
   getLikedPost,
   likeUnlikePost,
+  getFollowingPosts,
+  getUserPosts,
 } from "../controller/post.controller.js";
 
 const router = express.Router();
 
-router.get("/likes/:id", protectRoute, getLikedPost);
+router.get("/user/:username", protectRoute, getUserPosts); // post of the user
+router.get("/following", protectRoute, getFollowingPosts); // post of the user that we follow
+router.get("/likes/:id", protectRoute, getLikedPost); // liked post of the users
 router.get("/all", protectRoute, getAllPost);
 router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
