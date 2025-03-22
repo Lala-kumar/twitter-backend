e532cbab029104bdd4924a31c4b9b76a70313c21
Lala Kumar <lalak4099@gmail.com>
2025-03-22 17:45:39 +0530

    feat: update cookie SameSite attribute to 'none' for cross-origin requests

--- backend/utils/generateTokenAndSendCookie.js
+++ backend/utils/generateTokenAndSendCookie.js
@@ -8,7 +8,7 @@ const generateTokenAndSendCookie = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true, // xss attack
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV !== "development",
   });
 };
