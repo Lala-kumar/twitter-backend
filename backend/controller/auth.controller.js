22f64ff26759f67d7a6637bc0ca1bdc5796b16d9
Lala Kumar <lalak4099@gmail.com>
2025-03-25 09:49:43 +0530

    feat: update logout function to clear JWT cookie with secure and SameSite attributes

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -47,7 +47,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("jwt");
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
     res.status(200).json({
       message: "Logout successfully",
     });
